17bf524cff5b69ac6017d1771740cca69ce6c14b
Arifa Maulana Yusuf <suffstok70@gmail.com>
2025-08-25 13:55:24 +0700

    fungsi edit

--- script.js
+++ script.js
@@ -18,17 +18,27 @@ function tambahBaris() {
   baris.insertCell(2).textContent = email;
 
   const aksiCell = baris.insertCell(3);
+  const tombolEdit = document.createElement("button");
+  tombolEdit.textContent = "Edit";
+  tombolEdit.className = "edit-btn";
+  tombolEdit.onclick = function () {
+    document.getElementById("nama").value = baris.cells[0].textContent;
+    document.getElementById("umur").value = baris.cells[1].textContent;
+    document.getElementById("email").value = baris.cells[2].textContent;
+    tabelBody.removeChild(baris);
+  };
   const tombolHapus = document.createElement("button");
   tombolHapus.textContent = "Hapus";
   tombolHapus.className = "delete-btn";
   tombolHapus.onclick = function () {
     tabelBody.removeChild(baris);
   };
+
+  aksiCell.appendChild(tombolEdit);
   aksiCell.appendChild(tombolHapus);
+  
 
   document.getElementById("nama").value = "";
-  document.getElementById("umur").value = "";  
+  document.getElementById("umur").value = "";
   document.getElementById("email").value = "";
 }
-
-
